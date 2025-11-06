451a50995f45a403ee7e277240dba82d68683f15
TAMAS PETER BATTONYAI JUNIOR <tamas.battonyai@bradesco.com.br>
2025-11-06 10:53:06 -0300

    Componentes da Arquitetura Hexagonal

---
+++ 0x05/hexagonal_architecture/src/adapters/InMemoryUserRepository.java
@@ -0,0 +1,25 @@
+// Adaptador (Implementação do repositório em memória)
+package adapters;
+import domain.User;
+import ports.UserRepository;
+import java.util.*;
+
+
+public class InMemoryUserRepository implements UserRepository {
+    private final Map<String, User> database = new HashMap<>();
+
+    @Override
+    public void save(User user) {
+        database.put(user.getId(), user);
+    }
+
+    @Override
+    public User findById(String id) {
+        return database.get(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(database.values());
+    }
+}
